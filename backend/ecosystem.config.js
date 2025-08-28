df97fd8597c9f355d838716e2f4a0da2bb274a3b
Barriga <ricardo@feiraodigital.com.br>
2025-08-28 18:39:43 -0300

    Configs

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -2,18 +2,18 @@ module.exports = {
   apps: [
     {
       name: 'whaticket-backend',
-      script: './dist/server.js', // O script a ser executado
+      script: 'D:/@Projetos/WhatsApp/Whaticket/backend/dist/server.js', // O script a ser executado
       
-      // Define o diretÛrio de trabalho para a pasta raiz do backend.
-      // Isso garante que o '.env' seja encontrado no local esperado pelo cÛdigo.
-      cwd: './', 
+      // Define o diret√≥rio de trabalho para a pasta raiz do backend.
+      // Isso garante que o '.env' seja encontrado no local esperado pelo c√≥digo.
+      cwd: 'D:/@Projetos/WhatsApp/Whaticket/backend/', 
 
-      // Se a abordagem 'cwd' n„o funcionar, esta È ainda mais explÌcita.
-      // O PM2 ir· carregar este arquivo .env antes de iniciar o script.
-      // Descomente a linha abaixo se necess·rio, mas 'cwd' geralmente resolve.
+      // Se a abordagem 'cwd' n√£o funcionar, esta √© ainda mais expl√≠cita.
+      // O PM2 ir√° carregar este arquivo .env antes de iniciar o script.
+      // Descomente a linha abaixo se necess√°rio, mas 'cwd' geralmente resolve.
       // env_file: './.env',
 
-      // Opcional: modo cluster para usar todos os n˙cleos de CPU
+      // Opcional: modo cluster para usar todos os n√∫cleos de CPU
       // instances: 'max', 
       // exec_mode: 'cluster',
     },
